868613c157e97edd7a7abae8991053ee807cd717
ragingwind <ragingwind@gmail.com>
2016-07-13 10:44:19 +0900

    Update string syntax

--- index.js
+++ index.js
@@ -3,5 +3,5 @@
 module.exports = function (flag) {
 	// in false, hide. in true, show all files on desktop
 	var show = flag === 'show' ? 'true' : 'false';
-	require('child_process').exec("defaults write com.apple.finder CreateDesktop -bool " + show  + " && killall Finder");
+	require('child_process').exec(`defaults write com.apple.finder CreateDesktop -bool ${show} && killall Finder`);
 };
